78bad95493e4b0cb85399afc5c41d290bdcf3b10
John Galyon <jgalyon@covhlth.com>
2016-09-08 17:10:43 -0400

    First commit.

---
+++ clinician-custom-post.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Movie Reviews
+ * Plugin URI: http://wp.tutsplus.com/
+ * Description: Declares a plugin that will create a custom post type for Covenant Therapy Centers clinicians
+ * Version: 1.0
+ * Author: John Galyon
+ * Author URI: https://github.com/galyonj
+ * License: GPLv2
+ */
+?>
