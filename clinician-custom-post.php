be7c45502fb13f0b94bd0eae90560a1f17334e6c
John Galyon <jgalyon@covhlth.com>
2016-09-08 21:34:41 -0400

    First commit.

--- clinician-custom-post.php
+++ clinician-custom-post.php
@@ -9,59 +9,6 @@
  * License: GPLv2
  */
 
-// Create a custom post type to handle the addition of Covenant Therapy Centers clinicians
-/*add_action( 'init','clinicians_post_type', 0 );
-function clinicians_post_type() {
-	$labels = [
-		'add_new'               => 'Add New',
-		'add_new_item'          => 'Add New Clinician',
-		'all_items'             => 'All Clinicians',
-		'edit_item'             => 'Edit Clinician',
-		'featured_image'        => 'Featured Image',
-		'filter_items_list'     => 'Filter items list',
-		'insert_into_item'      => 'Insert into item',
-		'items_list'            => 'Items list',
-		'items_list_navigation' => 'Items list navigation',
-		'menu_admin_bar'        => 'Post Type',
-		'menu_name'             => 'Clinician Bios',
-		'name'                  => 'Clinician Bios',
-		'new_item'              => 'New Clinician',
-		'not_found'             => 'Not found',
-		'not_found_in_trash'    => 'Not found in Trash',
-		'parent_item_colon'     => 'Parent Item:',
-		'remove_featured_image' => 'Remove featured image',
-		'search_item'           => 'Search Item',
-		'set_featured_image'    => 'Set featured image',
-		'singular_name'         => 'Clinician',
-		'update_item'           => 'Update Item',
-		'uploaded_to_this_item' => 'Uploaded to this item',
-		'use_featured_image'    => 'Use as featured image',
-		'view_item'             => 'View Item'
-	];
-	$args = [
-		'can_export'          => true,
-		'capability_type'     => 'post',
-		'description'         => 'Create, edit, or delete Covenant Therapy Centers clinician biographies',
-		'exclude_from_search' => true,
-		'has_archive'         => false,
-		'hierarchical'        => false,
-		'label'               => 'clinician',
-		'labels'              => $labels,
-		'menu_icon'           => 'dashicons-welcome-write-blog',
-		'menu_position'       => 5,
-		'public'              => true,
-		'publicly_queryable'  => true,
-		'show_in_admin_bar'   => true,
-		'show_in_menu'        => true,
-		'show_in_nav_menus'   => false,
-		'show_ui'             => true,
-		'supports'            => [ 'title', 'editor', 'thumbnail', 'revisions' ],
-		'taxonomies'          => [ 'clinician_location' ]
-	];
-
-	register_post_type( 'clinician_bios', $args );
-}*/
-
 // Register Custom Post Type
 function clinicians_post_type() {
 
